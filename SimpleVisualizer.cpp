9137ed7d3efc80b13d038354d7445c6c4e2bbae1
PubuduS <pubudusupun@gmail.com>
2020-12-13 21:17:47 -0600

    Add Doxygen support.
    Add Doxygen to generate documentation and
    map the program. Also, add some dummy
    comment blocks to the program. Please
    add meaningful comments later.

--- SimpleVisualizer.cpp
+++ SimpleVisualizer.cpp
@@ -3,19 +3,45 @@
 #include "SimpleVisualizer.h"
 #include "CoutColor.h"
 
-
-//Constructor
+/*******************************************************************************
+ * A brief history of JavaDoc-style (C-style) banner comments.
+ *
+ * This is the typical JavaDoc-style C-style "banner" comment. It starts with
+ * a forward slash followed by some number, n, of asterisks, where n > 2. It's
+ * written this way to be more "visible" to developers who are reading the
+ * source code.
+ *
+ * Often, developers are unaware that this is not (by default) a valid Doxygen
+ * comment block!
+ *
+ * However, as long as JAVADOC_BLOCK = YES is added to the Doxyfile, it will
+ * work as expected.
+ *
+ * This style of commenting behaves well with clang-format.
+ *
+ * @param theory Even if there is only one possible unified theory. it is just a
+ *               set of rules and equations.
+ ******************************************************************************/
+ 
+//! The constructor.
 SimpleVisualizer :: SimpleVisualizer() :iterationCount(1)
 {
 
 }
 
-//De-constructor
+//! The destructor.
 SimpleVisualizer :: ~SimpleVisualizer()
 {
 
 }
 
+//! A normal member taking two arguments and returning an integer value.
+/*!
+\param a an integer argument.
+\param s a constant character pointer.
+\return The test results
+\sa QTstyle_Test(), ~QTstyle_Test(), testMeToo() and publicVar()
+*/
 void SimpleVisualizer :: drawArray( int* pNumArray, const int length, const int first, const int sorted, bool sortedFlag = false )
 {
    int second = first+1;
